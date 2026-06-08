4d1284f8a76278689d0060a3b3623b1d4475a8cf
Mridual-code <mridual.14910@stu.upes.ac.in>
2026-06-08 15:52:14 +0530

    completing my task

--- javascript-practice/functions.js
+++ javascript-practice/functions.js
@@ -1,4 +1,10 @@
+//function declaration 1:
 function myself(){
     console.lof("my name is Mridul")
 }
 myself()
+ //function declaration 2:
+function add(a,b){
+    return a+b
+}
+console.log(add(5,10))
